91afd6467edbb9a45eac8f35b433c77ade899fed
linesh <linesh.simpcity@gmail.com>
2016-06-09 17:53:11 +0800

    Linesh: Add slf4j logger and remove an unempty constructor

--- smart-core/src/main/java/com/thoughtworks/mvc/smart/core/CoreConfigurationReader.java
+++ smart-core/src/main/java/com/thoughtworks/mvc/smart/core/CoreConfigurationReader.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.mvc.smart.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -7,24 +10,25 @@ import java.util.Properties;
 public class CoreConfigurationReader {
 
     private Properties smartCoreConfiguration;
-    public static final String SMART_FRAMEWORK_PROPERTY_PREFIX = "smart.framework.app.";
-
-    public CoreConfigurationReader(Properties smartCoreConfiguration) {
-        this.smartCoreConfiguration = smartCoreConfiguration;
-    }
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public CoreConfigurationReader() {
+        initCoreConfigurationFromPropertyFile();
+    }
+
+    private void initCoreConfigurationFromPropertyFile() {
         InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("smart.properties");
         smartCoreConfiguration = new Properties();
         try {
             smartCoreConfiguration.load(stream);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.error("No configuration file 'smart.properties' found in resource root: src/main/resources");
         } finally {
             try {
                 stream.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                LOGGER.warn("Not able to close an input stream used to read 'smart.properties', reasons below: "
+                    + e.getMessage());
             }
         }
     }
@@ -32,4 +36,6 @@ public class CoreConfigurationReader {
     public String getBasePackage() {
         return (String) smartCoreConfiguration.get(SMART_FRAMEWORK_PROPERTY_PREFIX + "base-package");
     }
+
+    public static final String SMART_FRAMEWORK_PROPERTY_PREFIX = "smart.framework.app.";
 }
