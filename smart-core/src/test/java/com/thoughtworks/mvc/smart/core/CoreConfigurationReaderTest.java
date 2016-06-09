b0f001206099cc4df0a2e5aa19d105d0dca04084
linesh <linesh.simpcity@gmail.com>
2016-06-09 17:59:09 +0800

    Linesh: Refactor test, extract test target class initialization

--- smart-core/src/test/java/com/thoughtworks/mvc/smart/core/CoreConfigurationReaderTest.java
+++ smart-core/src/test/java/com/thoughtworks/mvc/smart/core/CoreConfigurationReaderTest.java
@@ -1,22 +1,27 @@
 package com.thoughtworks.mvc.smart.core;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class CoreConfigurationReaderTest {
+
+    private CoreConfigurationReader reader;
+
+    @Before
+    public void setUp() throws Exception {
+        reader = new CoreConfigurationReader();
+    }
+
     @Test
     public void should_return_correct_base_package_configuration_when_read() throws Exception {
-        CoreConfigurationReader reader = new CoreConfigurationReader();
-
         assertThat(reader.getBasePackage(), is("com.thoughtworks.opensource.demo"));
     }
 
     @Test
     public void should_return_WEB_INF_view_when_read_jsp_file_path_configuration() throws Exception {
-        CoreConfigurationReader reader = new CoreConfigurationReader();
-
         assertThat(reader.getJspPath(), is("/WEB-INF/views"));
     }
 }
