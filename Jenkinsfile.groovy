2e5ef622674bc3ad7cf1d0f3ad8a28e9b7c66bfc
linesh <linesh.simpcity@gmail.com>
2016-06-20 12:24:07 +0800

    Linesh: Specifying git configurations in jenkinsfile and I don't know why

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,10 @@
-node () {
+node ('test') {
     stage 'Run tests'
+    git([
+        url: 'git@github.com:linesh-simplicity/smart-light-weight-javaweb-framework.git',
+        branch: 'master'
+    ])
+
     def mavenHome = tool("Maven 3")
     env.PATH = "${mavenHome}/bin:${env.PATH}"
     sh 'mvn test'
