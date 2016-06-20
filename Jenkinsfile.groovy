740684aa37f83cbdb94e917177df2e1fa36401af
linesh <linesh.simpcity@gmail.com>
2016-06-20 11:06:57 +0800

    Linesh: Update jenkins file, add git repo url and maven path

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,6 @@
 node () {
     stage 'Run tests'
+    git url: 'git@github.com:linesh-simplicity/smart-light-weight-javaweb-framework.git'
+    env.PATH = "${tool 'Maven 3'}/bin:${env.PATH}"
     sh 'mvn test'
 }
