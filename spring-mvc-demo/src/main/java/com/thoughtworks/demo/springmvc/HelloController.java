38405bcdca7c325a1dc12ebb6a68e7814f9443b5
linesh <linesh.simpcity@gmail.com>
2016-06-20 18:01:37 +0800

    Linesh: Try as much as I can to config spring mvc to work, final tweak

---
+++ spring-mvc-demo/src/main/java/com/thoughtworks/demo/springmvc/HelloController.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.demo.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "hello";
+    }
+}
