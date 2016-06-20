c86637c4d23eb236f3dafcdf0fbc71096c6d6afb
linesh <linesh.simpcity@gmail.com>
2016-06-20 12:29:09 +0800

    Linesh: Finally it works. Now try maven test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,12 @@
 node ('main') {
 
-    stage 'Say Hello'
-    echo 'hello from pipeline'
-
     stage 'Run tests'
+    git([
+        url: 'git@github.com:linesh-simplicity/smart-light-weight-javaweb-framework.git',
+        branch: 'master'
+    ])
 
-//    git([
-//        url: 'git@github.com:linesh-simplicity/smart-light-weight-javaweb-framework.git',
-//        branch: 'master'
-//    ])
-//
-//    def mavenHome = tool("Maven 3")
-//    env.PATH = "${mavenHome}/bin:${env.PATH}"
-//    sh 'mvn test'
+    def mavenHome = tool("Maven 3")
+    env.PATH = "${mavenHome}/bin:${env.PATH}"
+    sh 'mvn test'
 }
