2c00ef5a4b0590da672bc8bde1a24709810a0c0e
linesh <linesh.simpcity@gmail.com>
2016-06-20 11:09:13 +0800

    Linesh: No idea what to do with jenkins scripts

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,7 @@
 node () {
     stage 'Run tests'
     git url: 'git@github.com:linesh-simplicity/smart-light-weight-javaweb-framework.git'
+    checkout scm
     env.PATH = "${tool 'Maven 3'}/bin:${env.PATH}"
     sh 'mvn test'
 }
