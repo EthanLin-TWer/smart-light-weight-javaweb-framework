275998d924bf6b238cb0fafaabec6860ee9add73
linesh <linesh.simpcity@gmail.com>
2016-06-20 12:26:39 +0800

    Linesh: Try simplest demo, an echo bash command to use jenkins pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,16 @@
 node ('test') {
-    stage 'Run tests'
-    git([
-        url: 'git@github.com:linesh-simplicity/smart-light-weight-javaweb-framework.git',
-        branch: 'master'
-    ])
 
-    def mavenHome = tool("Maven 3")
-    env.PATH = "${mavenHome}/bin:${env.PATH}"
-    sh 'mvn test'
+    stage 'Say Hello'
+    echo 'hello from pipeline'
+
+    stage 'Run tests'
+
+//    git([
+//        url: 'git@github.com:linesh-simplicity/smart-light-weight-javaweb-framework.git',
+//        branch: 'master'
+//    ])
+//
+//    def mavenHome = tool("Maven 3")
+//    env.PATH = "${mavenHome}/bin:${env.PATH}"
+//    sh 'mvn test'
 }
